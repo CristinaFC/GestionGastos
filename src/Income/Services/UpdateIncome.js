10e7b0a6a49160ab7c22be2f4049e73f59842972
Cristina Falcón Carque <cristina.falcon@singulardevs.com>
2024-04-24 18:37:33 +0100

    refactor to concept

--- src/Income/Services/UpdateIncome.js
+++ src/Income/Services/UpdateIncome.js
@@ -3,10 +3,10 @@ const Income = require('../Model/Income');
 
 const updateIncome = async (incomeId, body, user) =>
 {
-    const { date, amount, account, category, description, fixed } = body;
+    const { date, amount, account, category, concept } = body;
     const income = await Income.findByIdAndUpdate(
         incomeId,
-        { date, amount, account, category, description, user, fixed },
+        { date, amount, account, category, concept, user },
         { new: true },
     )
 
